82400c9a8b0b0f23ed67834767e55a8d2be3d5f1
pang0lin <pang0lin@163.com>
2023-01-16 10:48:04 +0800

    修改超时时间

--- utils/functions.py
+++ utils/functions.py
@@ -82,7 +82,7 @@ def copyJavaFile(srcpath, destpath):
 def execute(cmd):
     try:
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
-        proc.wait(timeout=30)
+        proc.wait(timeout=120)
         if platform.system() == "Windows":
             encoding = "gbk"
         else:
